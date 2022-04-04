75af9745bd14668035298923b35864d7e3de1011
J <joshdallos@gmail.com>
2022-04-04 17:50:11 +0300

    deployment prep

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-Inspir@tion.
-Created by J Dallos.
+# Inspir@tion.
+### Created by J Dallos.
 
 Inspir@tion is a web application utilizing the Bored API to provide the user with suggestions for activites.
 At the most basic level the app is an idea generator with a simple and playful user interface to encourage engagement.
 The app also provides a search function to allow the user to specify the parameters of their desired activities.
 
-Future plans for Inspir@tion include CRUD functionality, providing users with a unqiue session and storing their 'liked' activities 
+Future plans for Inspir@tion include CRUD functionality, providing users with a unqiue session and storing their 'liked' activities
 in a database in order to create a 'bucketlist'. Also a recommended search feature will be implemented based upon their 'liked' activities.
 
 
--- index.js
+++ index.js
@@ -11,19 +11,19 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  const title= 'Inspiration - Home';
+  const title = "Inspiration - Home";
 
   res.render("inspiration", { title });
 });
 
-app.get('/about', (req, res) =>{
-  const title = 'Inspiration - What is Inspiration?';
+app.get("/about", (req, res) => {
+  const title = "Inspiration - What is Inspiration?";
 
-  res.render('about', {title});
-})
+  res.render("about", { title });
+});
 
 app.get("/ideas", (req, res) => {
-  const title= 'Inspiration - Search';
+  const title = "Inspiration - Search";
 
   res.render("ideasForm", { title });
 });
@@ -34,78 +34,76 @@ app.post("/ideas", async (req, res) => {
   let maxPrice = 1;
   let minAccessibility = 0;
   let maxAccessibility = 1;
-  const numOfParticipants = [2,3,4,5,8];
+  const numOfParticipants = [2, 3, 4, 5, 8];
   let participants = 1;
-  let price = '';
-  let people = '';
-  let accessibility = '';
+  let price = "";
+  let people = "";
+  let accessibility = "";
   let searchInput = false;
 
-    if(type){
-      searchInput = true;
-    }
+  if (type) {
+    searchInput = true;
+  }
 
-    if (req.body.price === "free") {
-      minPrice = 0.1;
-      maxPrice = 0.6;
-      price = 'free';
-      searchInput = true;
-    }
-    if (req.body.price === "cheap") {
-      minPrice = 0.1;
-      maxPrice = 0.6;
-      price = 'cheap';
-      searchInput = true;
-    }
-    if (req.body.price === "luxury") {
-      // seems like api only works with 0.4 as the max min price...
-      minPrice = 0.4;
-      maxPrice = 1;
-      price = 'luxury';
-      searchInput = true;
-    }
+  if (req.body.price === "free") {
+    minPrice = 0.1;
+    maxPrice = 0.6;
+    price = "free";
+    searchInput = true;
+  }
+  if (req.body.price === "cheap") {
+    minPrice = 0.1;
+    maxPrice = 0.6;
+    price = "cheap";
+    searchInput = true;
+  }
+  if (req.body.price === "luxury") {
+    // seems like api only works with 0.4 as the max min price...
+    minPrice = 0.4;
+    maxPrice = 1;
+    price = "luxury";
+    searchInput = true;
+  }
 
-    if(req.body.accessibility === 'easy'){
-      minAccessibility = 0;
-      maxAccessibility = 0.2;
-      accessibility = 'easy';
-      searchInput = true;
-    }
-    if(req.body.accessibility === 'medium'){
-      minAccessibility = 0.2;
-      maxAccessibility = 0.6;
-      accessibility = 'medium';
-      searchInput = true;
-    }
+  if (req.body.accessibility === "easy") {
+    minAccessibility = 0;
+    maxAccessibility = 0.2;
+    accessibility = "easy";
+    searchInput = true;
+  }
+  if (req.body.accessibility === "medium") {
+    minAccessibility = 0.2;
+    maxAccessibility = 0.6;
+    accessibility = "medium";
+    searchInput = true;
+  }
 
-    if(req.body.accessibility === 'hard'){
-      minAccessibility = 0.6;
-      maxAccessibility = 1;
-      accessibility = 'hard';
-      searchInput = true;
-    }
+  if (req.body.accessibility === "hard") {
+    minAccessibility = 0.6;
+    maxAccessibility = 1;
+    accessibility = "hard";
+    searchInput = true;
+  }
 
-    if(req.body.participants === 'alone'){
-      people = 'alone';
-      searchInput = true;
-    }
+  if (req.body.participants === "alone") {
+    people = "alone";
+    searchInput = true;
+  }
 
-    if(req.body.participants === 'notAlone'){
+  if (req.body.participants === "notAlone") {
+    participants = numOfParticipants[Math.floor(Math.random() * 4)];
+    people = "not alone";
+    searchInput = true;
+  }
 
-      participants = numOfParticipants[Math.floor(Math.random()*4)];
-      people = 'not alone';
-      searchInput = true;
-
-    }
-  
   const getQuote = async () => {
     try {
       const res = await axios.get(
         `http://www.boredapi.com/api/activity?type=${type}&minprice=${minPrice}&maxprice=${maxPrice}&minaccessibility=${minAccessibility}&maxaccessibility=${maxAccessibility}&participants=${participants}`
       );
 
-      if(res.data.activity === undefined){
-        return "Sorry, we couldn't find a match. Try a different search."
+      if (res.data.activity === undefined) {
+        return "Sorry, we couldn't find a match. Try a different search.";
       }
 
       return res.data.activity;
@@ -115,12 +113,20 @@ app.post("/ideas", async (req, res) => {
   };
 
   const quote = await getQuote();
-  const title= 'Inspiration - Results';
-  res.render("ideasDisplay", { type, people, accessibility, price, quote, title, searchInput });
+  const title = "Inspiration - Results";
+  res.render("ideasDisplay", {
+    type,
+    people,
+    accessibility,
+    price,
+    quote,
+    title,
+    searchInput,
+  });
 });
 
+const port = process.env.PORT || 3000;
 
-
-app.listen(3000, () => {
-  console.log("listening on 3000");
+app.listen(port, () => {
+  console.log("Server started successfully");
 });
